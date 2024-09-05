bf04dcf7948f1bd715a149bb101780d8b1bfded1
Thanawat <stangthanawat2546@gmail.com>
2024-09-06 00:49:45 +0700

    060924

--- record.py
+++ record.py
@@ -1,52 +1,29 @@
 import cv2
 import time
-from datetime import datetime
+import os
 
-# Define start and end times
-start_time = "09:00"
-end_time = "10:00"
+# Create the folder if it doesn't exist
+if not os.path.exists('picSave'):
+    os.makedirs('picSave')
 
-# start_time = "08:10"
-# end_time = "10:11"
-# Function to check if the current time is within the recording period
-def within_recording_time(start_time, end_time):
-    current_time = datetime.now().strftime("%H:%M")
-    return start_time <= current_time < end_time
+vdo = cv2.VideoCapture('rtsp://admin:Admin123456@192.168.1.104:554/cam/realmonitor?channel=1&subtype=0&unicast=true&proto=Onvif')
 
-# Initialize video capture (0 for webcam, or provide a video file path)
-cap = cv2.VideoCapture('rtsp://admin:Admin123456@192.168.1.100:554/cam/realmonitor?channel=1&subtype=0&unicast=true&proto=Onvif')
+last_save_time = time.time()
 
-# Get the width and height of the video frames
-frame_width = int(cap.get(3))
-frame_height = int(cap.get(4))
-
-# Wait until the start time
-while datetime.now().strftime("%H:%M") != start_time:
-    print("Waiting to start recording...")
-    time.sleep(1)  # Sleep for 1 second to prevent busy-waiting
-
-# Create a timestamped filename
-timestamp = datetime.now().strftime("%d-%m-%Y_%H-%M")
-filename = f'vdoSave/{timestamp}.avi'
-
-# Define the codec and create a VideoWriter object
-fourcc = cv2.VideoWriter_fourcc(*'XVID')
-out = cv2.VideoWriter(filename, fourcc, 20.0, (frame_width, frame_height))
-
-print("Recording started...")
-
-# Start recording until the end time
-while within_recording_time(start_time, end_time):
-    ret, frame = cap.read()
+while True:
+    ret, pic = vdo.read()
+    print(last_save_time)
     if ret:
-        # Write the frame to the output file
-        out.write(frame)
-    else:
+        cv2.imshow('ff', pic)
+
+        # Save a picture every 10 seconds
+        if time.time() - last_save_time >= 10:
+            filename = f'vdoSave/frame_{int(time.time())}.jpg'
+            cv2.imwrite(filename, pic)
+            last_save_time = time.time()
+
+    if cv2.waitKey(1) & 0xFF == ord('p'):
         break
 
-print("Recording ended...")
-
-# Release everything when done
-cap.release()
-out.release()
+vdo.release()
 cv2.destroyAllWindows()
