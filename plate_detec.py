8a48a81dcaa561ba27624ddfe78063f5074c7bf3
Thanawat <stangthanawat2546@gmail.com>
2024-09-08 02:19:56 +0700

    080914

--- plate_detec.py
+++ plate_detec.py
@@ -1,14 +1,10 @@
 from ultralytics import YOLO
 import cv2 as cv
-import pyautogui
 import json
-from PIL import Image, ImageDraw, ImageFont
 import numpy as np
 import time
 from datetime import datetime
-import mysql.connector
 import os
-
 
 
 with open('class.json', 'r', encoding='utf-8') as file:
@@ -17,8 +13,8 @@ with open('class.json', 'r', encoding='utf-8') as file:
 model = YOLO('model/yolov8n.pt')
 modelP = YOLO('model/licen_100b.pt')
 modelC = YOLO('model/thaiChar_100b.pt')
-vdo = cv.VideoCapture('vdo_from_park/GF.mp4')
-# vdo = cv.VideoCapture('rtsp://admin:Admin123456@192.168.1.104:554/cam/realmonitor?channel=1&subtype=0&unicast=true&proto=Onvif')
+# vdo = cv.VideoCapture('vdo_from_park/GF.mp4')
+vdo = cv.VideoCapture('rtsp://admin:Admin123456@192.168.1.105:554/cam/realmonitor?channel=1&subtype=0&unicast=true&proto=Onvif')
 
 cv.namedWindow('Full Scene', cv.WND_PROP_FULLSCREEN)
 cv.setWindowProperty('Full Scene', cv.WND_PROP_FULLSCREEN, cv.WINDOW_FULLSCREEN)
@@ -39,7 +35,7 @@ datacar_in_park = []
 fps_start_time = time.time()
 fps_frame_count = 0
 line = []
-x_threshold=800
+x_threshold=700
 
 green = (0, 255, 0)  # empty
 red = (0, 0, 255)    # not empty
@@ -159,6 +155,9 @@ pic2 = pic.copy()
 
 if len(allline) < 2 :
     while check:
+        width = vdo.get(cv.CAP_PROP_FRAME_WIDTH)
+        height = vdo.get(cv.CAP_PROP_FRAME_HEIGHT)
+        cv.line(pic2,(x_threshold,0),(x_threshold,int(height)),red,2)
         cv.imshow("Full Scene", pic2)
         cv.setMouseCallback('Full Scene', mouse_click)
         if cv.waitKey(1) & 0xFF == ord('p'):
@@ -167,6 +166,9 @@ if len(allline) < 2 :
 while True:
     try:
         ret, pic = vdo.read()
+        width = vdo.get(cv.CAP_PROP_FRAME_WIDTH)
+        height = vdo.get(cv.CAP_PROP_FRAME_HEIGHT)
+
         if not ret:
             print("Failed to read frame. Exiting...")
             break
@@ -189,7 +191,8 @@ while True:
 
         cv.line(pic, (allline[0][0][0], allline[0][0][1]), (allline[0][1][0], allline[0][1][1]), yellow, 5)
         cv.line(pic, (allline[1][0][0], allline[1][0][1]), (allline[1][1][0], allline[1][1][1]), blue, 5)
-
+        cv.line(pic,(x_threshold,0),(x_threshold,int(height)),red,2)
+        
         result_model = model.track(pic_black, conf=0.5, classes=2, persist=True)
 
         for e in result_model[0].boxes:
